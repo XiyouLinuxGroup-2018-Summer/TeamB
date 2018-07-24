34c4c774309c737518630e977b2e404607331f2f
lalahaha323 <2665687186@qq.com>
2018-07-24 16:18:08 +0800

    add

---
+++ ACM/lcl/a.c
@@ -0,0 +1,39 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年07月24日 星期二 15时49分00秒
+ ************************************************************************/
+//--------A字符排序
+#include<stdio.h>
+int main(void)
+{
+    char num[3];
+    char a;
+    int i=0,j=0;
+    while(scanf("%s",num)!=EOF)
+    {
+        for(j=0;j<3;j++)
+        {
+            for(i=0;i<2;i++)
+            {
+                if(num[i]>num[i+1])
+                {
+                    a=num[i];
+                    num[i]=num[i+1];
+                    num[i+1]=a;
+                }
+            }
+        }
+        for(i=0;i<2;i++)
+            printf("%c ",num[i]);
+        printf("%c\n",num[2]);
+    }
+}
+
+
+
+
+
+
+
