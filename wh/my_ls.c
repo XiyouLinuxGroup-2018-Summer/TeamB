0370d7e37241915218535a3e4e2c35ba9a7490c3
wobushimotou4 <1687734258@qq.com>
2018-07-25 21:27:28 +0800

    ls -alR

--- wh/my_ls.c
+++ wh/my_ls.c
@@ -8,14 +8,14 @@
 #include<time.h>
 #include<grp.h>
 #include<pwd.h>
-void NpramNfile(void);							//无参数无文件
-void display_L(char *name);							
-void display_name(char *path);
-void display_R(char *name);
-void Sort(char ptr[][50],int length,int *a);	//按照字母排序
-void HpramNfile(char *pram);					//有参数无文件
-void NpramHfile(char ptr[][50],int count);				//无参数有文件
-void HpramHfile(char ptr[][50],int count,char *pram);	//有参数有文件
+void display_L(char *name);												//实现单个文件的ls -l 功能
+void display_name(char *path);											//实现ls -文件名/路径名 功能
+void display_R(char *name,int count);									//实现ls -R功能
+void display_RL(char *name,int count);									//实现ls -RL功能
+void Sort(char ptr[][50],int length,int *a);							//按照字母排序
+void HpramHNfile(char ptr[][50],int length,char *pram);					//有参数有无文件
+void NpramNfile(void);													//无参数无文件
+void NpramHfile(char ptr[][50],int count);								//无参数有文件
 
 int main(int argc,char *argv[])
 {
@@ -40,14 +40,10 @@ int main(int argc,char *argv[])
 	}
 	if(argc == 1)
 		NpramNfile();
-	else if(filename[0][0] == '0')
-		HpramNfile(pram);
-	else if(pram[0] = '0')
+	else if(pram[0] == '0')
 		NpramHfile(filename,count);
-//	if(argv[1][1] == 'R')
-//		display_R(filename[0]);
-//	else
-//		HpramHfile(filename,count,pram);
+	else
+		HpramHNfile(filename,count,pram);
 	return 0;
 }
 
@@ -82,54 +78,110 @@ void NpramNfile(void)
 	closedir(dir);
 	return;
 }
-//有参数，无文件的ls命令处理函数
-void HpramNfile(char *pram)
+
+
+//有参数，有无文件的ls命令处理函数
+void HpramHNfile(char ptr[][50],int length,char *pram)
 {
-	DIR *dir;
-	struct dirent *ptr;
-	char file[20][50];
-	int count = 0;
-	char path[] = ".";
-	if((dir = opendir(path)) == NULL) {
-		printf("open the %s failed",path);
-		return;
+	//处理无文件
+	if(length == 0) {
+		strcpy(ptr[0],".");
+		length = 1;
 	}
-	while((ptr = readdir(dir)) != NULL) {
-		strcpy(file[count++],ptr->d_name);
-	}
-
-	int ar[count],i;
-	for(i = 0;i < count;i++)
-		ar[i] = i;
-
-	Sort(file,count,ar);
-	
-	int flag = 0;
-	for(i = 0;i < strlen(pram);i++) {	//处理命令行参数
-		if(pram[i] == 'l')
-			flag |= 0010;
-		if(pram[i] == 'a')
-			flag |= 0001;
-	}
-
-	for(i = 0;i < count;i++)
-		switch(flag) {
-			case 0010:
-				if(file[ar[i]][0] != '.') {
-					display_L(file[ar[i]]);
-					putchar('\n');
-				}
-				break;
-			case 0001:
-				printf(" %s ",file[ar[i]]);
-				break;
-			case 0011:
-				display_L(file[ar[i]]);
-				putchar('\n');
-				break;
+	int i,j,k;								//循环变量
+	for(i = 0;i < length;i++) {
+		int count = 0;
+		int flag = 0;						//命令行参数转化为数字
+		DIR *dir;
+		struct dirent *str;
+		struct stat buf;
+		if(lstat(ptr[i],&buf) == -1) {
+			printf("error");
+			return;
 		}
-	putchar('\n');
+
+		if(S_ISDIR(buf.st_mode))			//判断是否为目录
+			count = 1;
+		
+		for(j = 0;j < strlen(pram);j++) { //获取命令行参数
+				if(pram[j] == 'l')
+					flag |= 0001;
+				if(pram[j] == 'R')
+					flag |= 0010;
+				if(pram[j] == 'a')
+					flag |= 0100;
+		}
+		
+		if(count) {
+			switch(flag) {
+				case 0100:					//ls -a + 目录
+					if((dir = opendir(ptr[i])) == NULL) {
+						return;
+					}
+					while((str = readdir(dir)) != NULL)
+							printf(" %s ",str->d_name);
+					closedir(dir);
+					break;
+								
+				case 0001:					//ls -l + 目录
+					if((dir = opendir(ptr[i])) == NULL) {
+						return;
+					}
+					chdir(ptr[i]);
+					while((str = readdir(dir)) != NULL)
+						if(str->d_name[0] != '.') {
+							display_L(str->d_name);
+							putchar('\n');
+						}
+					
+					closedir(dir);
+					break;
+				case 0101:					//ls -la + 目录
+					if((dir = opendir(ptr[i])) == NULL) {
+						return;
+					}
+					chdir(ptr[i]);
+					while((str = readdir(dir)) != NULL) {
+							display_L(str->d_name);
+							putchar('\n');
+					}
+					
+					closedir(dir);
+					break;
+				case 0010:
+					display_R(ptr[i],0);
+					break;
+				case 0110:
+					display_R(ptr[i],1);
+					break;
+				case 0011:
+					display_RL(ptr[i],0);
+					break;
+				case 0111:
+					display_RL(ptr[i],1);
+					break;
+			}		
+		}
+		else {
+			switch(flag) {
+				case 0100:					//ls -a + 文件
+				case 0101:
+				case 0001:
+					printf(" %s ",ptr[i]);
+					break;
+				case 0010:
+				case 0111:					//ls -l + 文件
+				case 0011:
+				case 0110:
+					display_L(ptr[i]);
+					putchar('\n');
+					break;
+			}
+		}
+	}
 }
+			
+					
 
 void NpramHfile(char ptr[][50],int count)				//无参数有文件
 {
@@ -137,13 +189,13 @@ void NpramHfile(char ptr[][50],int count)				//无参数有文件
 	for(i = 0;i < count;i++)
 		display_name(ptr[i]);
 }
-//根据文件名或目录路径显示信息
+
+//ls-文件名/路径
 void display_name(char *path)
 {
 	int i,j,count = 0;
-	for(i = 0;i < strlen(path);i++)
-		if(path[i] == '/')
-			count = 1;
+	if(path[strlen(path) - 1] == '/')
+		count = 1;
 	if(count) {
 		DIR *dir;
 		struct dirent *ptr;
@@ -168,13 +220,21 @@ void display_name(char *path)
 			
 	}
 	else {
-		printf(" %s ",path);
+		char name[20];
+		for(i = 0,j = 0;i < strlen(path);i++) {
+			if(path[i] == '/') {
+				j = 0;
+				continue;
+			}
+			name[j++] = path[i];
+		}
+		printf("%s",name);
 	}
 	return;
 }
 	
 
-//参数为 la 或 l 的一个文件
+//ls -l
 void display_L(char *name)
 {
 	char file_time[50];
@@ -259,46 +319,62 @@ void display_L(char *name)
 	
 }	
 
-//根据文件名实现ls -R功能
-void display_R(char *name)
+//ls -R 路径名
+void display_R(char *name,int count)
 {
-	struct stat *buf;
-	if(lstat(name,buf) == -1) {
+	struct stat buf;
+	if(lstat(name,&buf) == -1) {				//根据文件名获取信息
 		printf("open the %s is error",name);
 		return;
 	}
 	
-	if(S_ISDIR(buf->st_mode)) {
+	if(S_ISDIR(buf.st_mode)) {					//判断是否为目录
 		DIR *dir;
 		struct dirent *ptr;
 		if((dir = opendir(name)) == NULL) {
-			printf("open the %s is failed",name);
+			printf("open the %s is failed",name);//打开目录
 			return;
 		}
-
-		while((ptr = readdir(dir)) != NULL)
-			display_R(ptr->d_name);
+		chdir(name);								//进入下一级目录
+		while((ptr = readdir(dir)) != NULL)			//获取下一级信息
+			if(ptr->d_name[0] != '.')
+				display_R(ptr->d_name,count);			//根据下一级文件名递归遍历
+		chdir("..");							//遍历完下一级，返回上一级
+		closedir(dir);					
 	}
 	else
-		printf(" %s ",name);
+		if(name[0] != '.' || count)
+			printf(" %s ",name);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void display_RL(char *name,int count)					//实现ls -RL功能
+{
+	struct stat buf;
+	if(lstat(name,&buf) == -1) {				//根据文件名获取信息
+		printf("open the %s is error",name);
+		return;
+	}
+	
+	if(S_ISDIR(buf.st_mode)) {					//判断是否为目录
+		DIR *dir;
+		struct dirent *ptr;
+		if((dir = opendir(name)) == NULL) {
+			printf("open the %s is failed",name);//打开目录
+			return;
+		}
+		chdir(name);								//进入下一级目录
+		while((ptr = readdir(dir)) != NULL)			//获取下一级信息
+			if(ptr->d_name[0] != '.')
+				display_RL(ptr->d_name,count);			//根据下一级文件名递归遍历
+		chdir("..");							//遍历完下一级，返回上一级
+		closedir(dir);					
+	}
+	else
+		if(name[0] != '.' || count) {
+			display_L(name);
+			putchar('\n');
+		}
+}
 //对字符串进行排序,结果以游标形式保存在a数组中
 void Sort(char ptr[][50],int length,int *a)
 {
@@ -312,14 +388,4 @@ void Sort(char ptr[][50],int length,int *a)
 				a[j] = swep;
 			}
 }
-
-
-
-
-
-
-
-
-
-
 
