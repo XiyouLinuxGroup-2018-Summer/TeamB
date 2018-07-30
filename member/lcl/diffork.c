1c805dce2b422d4b89780000c99b09af7813bcef
lalahaha323 <2665687186@qq.com>
2018-07-30 21:02:36 +0800

    add diffork.c

---
+++ member/lcl/diffork.c
@@ -0,0 +1,54 @@
+/*************************************************************************
+	> File Name: diffork.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年07月30日 星期一 20时36分22秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+int globVar=5;
+int main(void)
+{
+    pid_t pid;
+    int var=1;
+    int i;
+    printf("fork is differernt with vfork\n");
+    pid=vfork();
+    switch(pid)
+    {
+        case 0:
+        {
+            i=3;
+            while(i-->0)
+            {
+                printf("child process is running\n");
+                globVar++;
+                var++;
+                sleep(1);
+            }
+            printf("child's globVar=%d,var=%d\n",globVar,var);
+            exit(0);
+        }
+        case -1:
+        {
+            perror("process creat default\n");
+            exit(0);
+        }
+        default:
+        {
+            i=5;
+            while(i-->0)
+            {
+                printf("parent process is running\n");
+                globVar++;
+                var++;
+                sleep(1);
+            }
+            printf("parent's globVar=%d,var=%d\n",globVar,var);
+            exit(0);
+        }
+    }
+}
