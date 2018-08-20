f8d9e3b78b0c7c3597b8854f8bedc152fd361735
wobushimotou4 <1687734258@qq.com>
2018-08-20 18:03:29 +0800

    wee

--- member/wh/par.c
+++ member/wh/par.c
@@ -3,10 +3,8 @@
 #include <time.h>
 int main(void)
 {
-	time_t timer;//time_t就是long int 类型
-	struct tm *tblock;
-	timer = time(NULL);//这一句也可以改成time(&timer);
-	tblock = localtime(&timer);
-	printf("Local time is: %s\n",asctime(tblock));
+	if(getchar() == 'q')
+		puts("hello");
 	return 0;
+
 }
